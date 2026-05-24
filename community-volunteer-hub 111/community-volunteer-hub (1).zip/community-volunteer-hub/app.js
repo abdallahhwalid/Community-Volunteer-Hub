7b2b6a163625c97ebdc13f3f654cfb35b0c8702e
abdallahhwalid <abdallahwalid2007@gmail.com>
2026-05-24 18:58:16 +0300

    complete project structure and also all empty files ready for team

---
+++ community-volunteer-hub 111/community-volunteer-hub (1).zip/community-volunteer-hub/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+require('dotenv').config();
+
+const app = express();
+
+// Set EJS as view engine
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+
+// Serve static files from public/
+app.use(express.static('public'));
+
+// Temporary test route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Connect to MongoDB FIRST, then start server
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 5000,
+  family: 4
+})
+  .then(() => {
+    console.log('MongoDB connected!');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('MongoDB connection error:', err);
+  });
